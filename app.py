21a3b9e9de21af77b3b6f7e0009c2faf0d243aef
mordern_geek <dyopal@gmail.com>
2021-11-19 08:31:36 +0100

    Update
    push missing vagrant

--- app.py
+++ app.py
@@ -124,4 +124,6 @@ def metrics():
 
 # start the application on port 3111
 if __name__ == "__main__":
+    
+   logging.basicConfig(filename='techtrends.log',level=logging.DEBUG) 
    app.run(host='0.0.0.0', port='3111')
